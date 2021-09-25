d4cbabbd6bcb7b73a0b5a91da72ce7973f3b6ff8
OmerRonen <omerronen10@gmail.com>
2021-09-25 09:49:29 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stat-215-a-gsi',
+    version='1.0',
+    packages=['gsi'],
+    url='',
+    license='',
+    author='omerronen',
+    author_email='omer_ronen@berkeley.edu',
+    description='215A gsi package'
+)
